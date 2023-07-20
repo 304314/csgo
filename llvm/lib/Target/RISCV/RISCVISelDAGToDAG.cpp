35f30fa3482881562cb29a5663e6d21e3884947a
liyunfei <liyunfei33@huawei.com>
2023-07-20 15:11:26 +0800

    Revert "[Backport][RISCV] Handle "o" inline asm memory constraint"
    This reverts commit a831cf03a32854ac193389819189e6ee93b0ecaf.

--- llvm/lib/Target/RISCV/RISCVISelDAGToDAG.cpp
+++ llvm/lib/Target/RISCV/RISCVISelDAGToDAG.cpp
@@ -1733,7 +1733,6 @@ void RISCVDAGToDAGISel::Select(SDNode *Node) {
 bool RISCVDAGToDAGISel::SelectInlineAsmMemoryOperand(
     const SDValue &Op, unsigned ConstraintID, std::vector<SDValue> &OutOps) {
   switch (ConstraintID) {
-  case InlineAsm::Constraint_o:
   case InlineAsm::Constraint_m:
     // We just support simple memory operands that have a single address
     // operand and need no special handling.
@@ -1743,8 +1742,7 @@ bool RISCVDAGToDAGISel::SelectInlineAsmMemoryOperand(
     OutOps.push_back(Op);
     return false;
   default:
-    report_fatal_error("Unexpected asm memory constraint " +
-                       InlineAsm::getMemConstraintName(ConstraintID));
+    break;
   }
 
   return true;
