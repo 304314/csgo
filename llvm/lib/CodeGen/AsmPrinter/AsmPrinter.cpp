8f6170e0fc60c61354bdcef0f1b3c1b619b9fb74
Mircea Trofin <mtrofin@google.com>
2023-09-19 14:37:06 -0700

    [AsmPrint] Dump raw frequencies in `-mbb-profile-dump` (#66818)
    We were losing the function entry count, which is useful to check profile quality. For the original cases where we want
    entrypoint-relative MBB frequencies, the user would just need to divide these values by the entrypoint (first MBB, with ID=0) value.

--- llvm/lib/CodeGen/AsmPrinter/AsmPrinter.cpp
+++ llvm/lib/CodeGen/AsmPrinter/AsmPrinter.cpp
@@ -1934,7 +1934,7 @@ void AsmPrinter::emitFunctionBody() {
     for (const auto &MBB : *MF) {
       *MBBProfileDumpFileOutput.get()
           << MF->getName() << "," << MBB.getBBID() << ","
-          << MBFI.getBlockFreqRelativeToEntryBlock(&MBB) << "\n";
+          << MBFI.getBlockFreq(&MBB).getFrequency() << "\n";
     }
   }
 }
