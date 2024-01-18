f5009ea4684a05ab080da6778d3a5fe1beb433f6
Matthew Devereau <matthew.devereau@arm.com>
2024-01-18 09:17:23 +0000

    [AArch64][SME] Conditionally do smstart/smstop (#77113)
    This patch adds conditional enabling/disabling of streaming mode for
    functions which have both the aarch64_pstate_sm_compatible and
    aarch64_pstate_sm_body attributes.
    
    This combination allows callees to determine if switching streaming mode
    is required instead of relying on the caller.
    
    Signed-off-by: chenmiao <15273704+chenmiao32@user.noreply.gitee.com>

--- llvm/lib/Target/AArch64/AArch64MachineFunctionInfo.h
+++ llvm/lib/Target/AArch64/AArch64MachineFunctionInfo.h
@@ -196,6 +196,10 @@ class AArch64FunctionInfo final : public MachineFunctionInfo {
 
   int64_t StackProbeSize = 0;
 
+  // Holds a register containing pstate.sm. This is set
+  // on function entry to record the initial pstate of a function.
+  Register PStateSMReg = MCRegister::NoRegister;
+
 public:
   AArch64FunctionInfo(const Function &F, const AArch64Subtarget *STI);
 
@@ -203,6 +207,9 @@ public:
   clone(BumpPtrAllocator &Allocator, MachineFunction &DestMF,
         const DenseMap<MachineBasicBlock *, MachineBasicBlock *> &Src2DstMBB)
       const override;
+
+  Register getPStateSMReg() const { return PStateSMReg; };
+  void setPStateSMReg(Register Reg) { PStateSMReg = Reg; };
 
   bool isSVECC() const { return IsSVECC; };
   void setIsSVECC(bool s) { IsSVECC = s; };
