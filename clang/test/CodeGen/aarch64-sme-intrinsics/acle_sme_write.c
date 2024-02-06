abb94c8cfeff729a94305b4708b5bde6cab6c1b6
Sander de Smalen <sander.desmalen@arm.com>
2024-02-06 10:42:44 +0000

    [Clang][AArch64] Fix some target guards and remove +sve from tests. (#80681)
    The TargetGuard fields for 'svldr[_vnum]_za' and 'svstr[_vnum]_za' were
    incorrectly set to `+sve` instead of `+sme`. This means that compiling
    code that uses these intrinsics requires compiling for both `+sve` as
    well as `+sme`.
    
    This PR also fixes the target guards for the `svadd` and `svsub`
    builtins that are enabled under `+sme2,+sme-i16i64` and
    `+sme2,+sme-f64f64`, as it initially did the following:
    ```
      let TargetGuard = "+sme2" in {
        let TargetGuard = "+sme-i16i64" in {
          // Builtins defined here will be predicated only by
          // '+sme-i16i64', and not '+sme2,+sme-i16i64'.
        }
      }
    ```
    This PR also removes `-target-feature +sve` from all the SME tests, to
    ensure that the SME features are sufficient to build the tests.
    
    Signed-off-by: chenmiao <15273704+chenmiao32@user.noreply.gitee.com>

--- clang/test/CodeGen/aarch64-sme-intrinsics/acle_sme_write.c
+++ clang/test/CodeGen/aarch64-sme-intrinsics/acle_sme_write.c
@@ -1,9 +1,9 @@
 // REQUIRES: aarch64-registered-target
-// RUN: %clang_cc1 -triple aarch64-none-linux-gnu -target-feature +sme -target-feature +sve -S -O1 -Werror -emit-llvm -o - %s | FileCheck %s -check-prefixes=CHECK,CHECK-C
-// RUN: %clang_cc1 -triple aarch64-none-linux-gnu -target-feature +sme -target-feature +sve -S -O1 -Werror -emit-llvm -o - -x c++ %s | FileCheck %s -check-prefixes=CHECK,CHECK-CXX
-// RUN: %clang_cc1 -DSME_OVERLOADED_FORMS -triple aarch64-none-linux-gnu -target-feature +sme -target-feature +sve -S -O1 -Werror -emit-llvm -o - %s | FileCheck %s -check-prefixes=CHECK,CHECK-C
-// RUN: %clang_cc1 -DSME_OVERLOADED_FORMS -triple aarch64-none-linux-gnu -target-feature +sme -target-feature +sve -S -O1 -Werror -emit-llvm -o - -x c++ %s | FileCheck %s -check-prefixes=CHECK,CHECK-CXX
-// RUN: %clang_cc1 -triple aarch64-none-linux-gnu -target-feature +sme -target-feature +sve -S -O1 -Werror -o /dev/null %s
+// RUN: %clang_cc1 -triple aarch64-none-linux-gnu -target-feature +sme -S -O1 -Werror -emit-llvm -o - %s | FileCheck %s -check-prefixes=CHECK,CHECK-C
+// RUN: %clang_cc1 -triple aarch64-none-linux-gnu -target-feature +sme -S -O1 -Werror -emit-llvm -o - -x c++ %s | FileCheck %s -check-prefixes=CHECK,CHECK-CXX
+// RUN: %clang_cc1 -DSME_OVERLOADED_FORMS -triple aarch64-none-linux-gnu -target-feature +sme -S -O1 -Werror -emit-llvm -o - %s | FileCheck %s -check-prefixes=CHECK,CHECK-C
+// RUN: %clang_cc1 -DSME_OVERLOADED_FORMS -triple aarch64-none-linux-gnu -target-feature +sme -S -O1 -Werror -emit-llvm -o - -x c++ %s | FileCheck %s -check-prefixes=CHECK,CHECK-CXX
+// RUN: %clang_cc1 -triple aarch64-none-linux-gnu -target-feature +sme -S -O1 -Werror -o /dev/null %s
 
 #include <arm_sme.h>
 
